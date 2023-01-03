471f1e100755103b25220c831ab3389827239e81
Jude <116462617+jude-kerr@users.noreply.github.com>
2023-01-03 04:06:00 +0000

    NTR-23 Full

--- nitrotumorremover.plugin.js
+++ nitrotumorremover.plugin.js
@@ -11,8 +11,6 @@ module.exports = class ExamplePlugin {
     start() {
         const element = document.querySelector("li.channel-1Shao0.container-32HW5s:nth-of-type(2) > .interactive-1vLZ_I.interactive-iyXY_x.linkButton-2NshQj > .link-39sEB3 > .avatarWithText-1tTt2S.layout-1LjVue");
 element.remove();
-      // Called when the plugin is activated (including after reloads)
-      BdApi.alert("This plugin is made to remove the Nitro button. If you need help please make an inquiry to sirjudith69@outlook.com");
     } 
 
     stop() {
