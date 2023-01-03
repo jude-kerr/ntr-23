f4834d13cdce038cadec6373de3ddaf6e632576f
Miles <116462617+jude-kerr@users.noreply.github.com>
2023-01-03 21:21:55 +0000

    NTR-23 Code

--- nitrotumorremover.plugin.js
+++ nitrotumorremover.plugin.js
@@ -2,19 +2,22 @@
  * @name NTR-23
  * @author Miles#5157
  * @description The Nitro Tumor Remover plugin for BetterDiscord. (Thanks to PickleNik for helping)
- * @version 0.0.1
+ * @version 0.0.3
  */
 
 
 
 module.exports = class ExamplePlugin {
     start() {
-        const element = document.querySelector("li.channel-1Shao0.container-32HW5s:nth-of-type(2) > .interactive-1vLZ_I.interactive-iyXY_x.linkButton-2NshQj > .link-39sEB3 > .avatarWithText-1tTt2S.layout-1LjVue");
-element.remove();
+        setInterval(() => {
+            const element = document.querySelector("li.channel-1Shao0.container-32HW5s:nth-of-type(2) > .interactive-1vLZ_I.interactive-iyXY_x.linkButton-2NshQj > .link-39sEB3 > .avatarWithText-1tTt2S.layout-1LjVue");
+            element.remove();
+          }, 10000)
+      // Called when the plugin is activated (including after reloads)
+      BdApi.alert("If you need help with the extension please vist: bit.ly/NTR-Plugin");
     } 
 
     stop() {
       // Called when the plugin is deactivated
     }
 }
- 
